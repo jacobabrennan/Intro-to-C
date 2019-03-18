5d248c13590f56445e99e426279fe5d5db08fa5b
JacobABrennan <jacobabrennan@gmail.com>
2019-03-18 22:01:03 +0100

    Finish fizzbuzz

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -8,9 +8,26 @@
     every time that nothing gets printed and return the counter.
     Don't forget to include newlines '\n' in your printf statements!
 */
-int fizzbuzz(int n)
+int fizzbuzz(int iterations)
 {
-
+    int non_zz = 0;
+    int index;
+    for(index=1; index<=iterations; index++)
+    {
+        if(((index%3)||(index%5))== 0) {
+            printf("FizzBuzz\n");
+        }
+        else if((index%3)==0) {
+            printf("Fizz\n");
+        }
+        else if((index%5)==0) {
+            printf("Buzz\n");
+        }
+        else {
+            non_zz++;
+        }
+    }
+    return non_zz;
 }
 
 #ifndef TESTING
