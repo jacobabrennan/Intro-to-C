38b309323963640818b7d3f0803ce587e2d9e1ea
JacobABrennan <jacobabrennan@gmail.com>
2019-03-18 22:27:17 -0400

    Finish pointers.c

--- pointers/pointers.c
+++ pointers/pointers.c
@@ -8,7 +8,13 @@
 */
 void string_copy(char *x, char *y)
 {
-
+    int index = 0;
+    do
+    {
+        x[index] = y[index];
+        index++;
+    }
+    while(y[index] != '\0');
 }
 
 /*
@@ -21,7 +27,14 @@ void string_copy(char *x, char *y)
 */
 char *find_char(char *str, int c)
 {
-
+    for(int index=0; str[index] != '\0'; index++)
+    {
+        if(str[index] == c)
+        {
+            return str+index;
+        }
+    }
+    return (char *) 0;
 }
 
 /*
@@ -34,14 +47,32 @@ char *find_char(char *str, int c)
 */
 char *find_string(char *haystack, char *needle)
 {
-
+    for(int pos_sub_str = 0; haystack[pos_sub_str] != '\0'; haystack++)
+    {
+        int match = 1;
+        for(int pos_test = 0; needle[pos_test] != '\0'; pos_test++)
+        {
+            char char_needle = needle[pos_test];
+            char char_haystack = haystack[pos_sub_str+pos_test];
+            if(char_needle != char_haystack)
+            {
+                match = 0;
+                break;
+            }
+        }
+        if(match)
+        {
+            return haystack+pos_sub_str;
+        }
+    }
+    return (char *) 0;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    char *found_char = find_char(hello, 'e');
-    char *found_string = find_string(world, "or");
+    char *found_char = find_char('hello', 'e');
+    char *found_string = find_string('world', "or");
 
     printf("Found char: %s\n", found_char);
     printf("Found string: %s\n", found_string);
